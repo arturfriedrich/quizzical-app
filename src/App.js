1edc90c6f4b0fbce66fa798ee06f9d415a68d9e2
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-02-01 11:23:39 +0100

    Start mechanism added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 
-import Start from "./components/Start"
+import Question from "./components/Question"
 
 export default function App() {
+
+    const [running, setRunning] = useState(false)
+
+    function startQuiz() {
+        setRunning(prevState => !prevState)
+    }
+
     return (
-        <>
-            <Start />
-        </>
+        <main>
+            {
+                running ?
+                    <Question />
+                    :
+                    <section className="start-screen">
+                        <h1 className="start-title">Quizzical</h1>
+                        <p className="start-description">Answer five questions to test<br /> your knowledge</p>
+                        <button className="start-button" onClick={startQuiz}>Start quiz</button>
+                    </section>
+            }
+        </main>
     )
 }
