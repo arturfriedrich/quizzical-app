1823130c55af14d170cd70336d444ab05158e689
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-02-01 11:07:52 +0100

    Start screen CSS added

--- src/components/Start.js
+++ src/components/Start.js
@@ -4,7 +4,7 @@ export default function Start() {
     return (
         <section className="start-screen">
             <h1 className="start-title">Quizzical</h1>
-            <p className="start-description">Answer five questions to test your knowledge</p>
+            <p className="start-description">Answer five questions to test<br /> your knowledge</p>
             <button className="start-button">Start quiz</button>
         </section>
     )
